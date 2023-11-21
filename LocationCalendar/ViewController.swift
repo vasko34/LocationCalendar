cfadb1ac31254051e943cc209b40522c7e1a0e41
vasko34 <56612508+vasko34@users.noreply.github.com>
2023-11-21 13:41:36 +0200

    Initial Commit

---
+++ LocationCalendar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationCalendar
+//
+//  Created by User on 21.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
